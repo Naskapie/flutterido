fb29a7b56c5b6b140db93d8845950b3f241aee22
Naskapie <naskapie@gmx.net>
2020-06-22 15:00:13 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutterido/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterido
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/task_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class TasksScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.lightBlueAccent,
+        body: Container(
+          padding: EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0),
+          child: Column(children: [
+            CircleAvatar(
+              child:
+                  Icon(Icons.list, size: 30.0, color: Colors.lightBlueAccent),
+              backgroundColor: Colors.white,
+              radius: 30.0,
+            ),
+            Text('Todoey'),
+          ]),
+        ));
+  }
+}
